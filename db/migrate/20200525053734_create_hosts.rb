101dc18241c5e31dcc8c43dec2dda39f140f7146
makino3303 <makino3303@icloud.com>
2020-06-03 00:05:38 +0900

    画像以外の登録機能の実装
    簡易的な登録機能の実装

--- db/migrate/20200525053734_create_hosts.rb
+++ db/migrate/20200525053734_create_hosts.rb
@@ -1,7 +1,11 @@
 class CreateHosts < ActiveRecord::Migration[5.2]
   def change
     create_table :hosts do |t|
-
+      t.string :title
+      t.string :food1
+      t.string :food2
+      t.string :food3
+      t.string :time
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 2020_05_25_053734) do
 
   create_table "hosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "food1"
+    t.string "food2"
+    t.string "food3"
+    t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
