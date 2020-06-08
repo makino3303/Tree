38b3fccf665ef6b5dcd2e6084db31dfea35b65af
makino3303 <makino3303@icloud.com>
2020-06-08 23:59:44 +0900

    詳細ページの設定
    詳細ページの簡易ページ作成

--- db/migrate/20200525053734_create_hosts.rb
+++ db/migrate/20200525053734_create_hosts.rb
@@ -5,6 +5,15 @@ class CreateHosts < ActiveRecord::Migration[5.2]
       t.string :food1
       t.string :food2
       t.string :food3
+      t.string :subfood1
+      t.string :subfood2
+      t.string :subfood3
+      t.string :seasoning1
+      t.string :seasoning2
+      t.string :seasoning3
+      t.string :seasoning4
+      t.string :seasoning5
+      t.string :seasoning6
       t.string :image
       t.string :time
       t.timestamps
